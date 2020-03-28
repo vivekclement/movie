c69f100dd636c5dbb3fe3c877fe1307208bcf971
vivekclement <vivekclement@gmail.com>
2020-03-29 01:29:31 +0800

    final cut

--- api/models.py
+++ api/models.py
@@ -7,6 +7,19 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.CharField(max_length=500)
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+        if len(ratings) > 0:
+            return sum/len(ratings)
+        else:
+            return 0
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
